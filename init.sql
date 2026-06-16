71944f438fb649c18ff927d961c1f372196ebdca
ryoubaiwarabe-creator <ryoubaiwarabe@gmail.com>
2026-06-16 17:54:11 +0900

    sql updated

--- init.sql
+++ init.sql
@@ -1,5 +1,59 @@
-CREATE DATABASE `task_agent_db`;
-USE `task_agent_db`;
-CREATE TABLE test_data(id INT PRIMARY KEY AUTO_INCREMENT, title VARCHAR(30) NOT NULL);
+CREATE "DATABASE task_agent_db";
 
-INSERT INTO test_data(title) VALUES('testdata1');
+USE `task_agent_db`;
+
+
+CREATE TABLE users (                    --ユーザーテーブル制作
+    name TEXT,
+    password_hash TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,  
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+INSERT INTO  users(name, password_hash, email)
+VALUES (
+        '前田',
+        'youkoso',
+        'example@gmail.com'
+
+);
+
+CREATE TABLE tasks (
+    CREATE TABLE test_data(id INT PRIMARY KEY AUTO_INCREMENT, title VARCHAR(30) NOT NULL)         
+    title TEXT NOT NULL,
+    comment TEXT,
+    limit_date DATE,
+    priority TEXT CHECK (priority IN ('高', '中', '小')),
+    completed BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO tasks (title, comment, limit_date, priority, completed)
+VALUES (
+
+    'DjangoのAPI掲示板を完成させる',         -- title
+    '先生に教えてもらう',                     -- comment
+    '2026-06-30',                           -- limit_date (期限日)
+    '高',                                   -- priority ('高', '中', '小')
+    FALSE                                   -- completed (まだ未完了)
+);
+INSERT INTO tasks ( title, priority) --空白はNULL
+VALUES (
+ 
+    'スーパーで牛乳を買う',                  
+    '中'                                    
+);
+CREATE TABLE task_name_history (
+    CREATE TABLE test_data(id INT PRIMARY KEY AUTO_INCREMENT, title VARCHAR(30) NOT NULL)
+    update_task TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+INSERT INTO task_name_history (
+    
+    update_task
+)
+VALUES (
+
+    'xammpの勉強会'
+);
