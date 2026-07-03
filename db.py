0faec96cf5f16188f6861a266251d8acfe86d9f9
ryoubaiwarabe-creator <ryoubaiwarabe@gmail.com>
2026-07-03 10:05:07 +0900

    Update db.py

--- db.py
+++ db.py
@@ -1,1 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+DATABASE_URL = "postgresql://postgres:warabe0323@localhost:5432/calendar_db"
 
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
