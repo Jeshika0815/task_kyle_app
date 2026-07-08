49548deb43679d391754ab0cedb34add9709a59b
Jeshika0815 <saito.jeshi.0815@gmail.com>
2026-07-08 14:54:57 +0900

    Improved CI/CD and docker relations

--- app/routers/prompt_organize.py
+++ app/routers/prompt_organize.py
@@ -1,12 +1,16 @@
-from fastapi import APIRouter, HTTPException, status
-import app.auth_relation as auth
+from fastapi import APIRouter, HTTPException, status, Depends
+from app.auth_relation import token_verification as token_veri
 import app.prompt_listing as ppl
+from app.auth_relation import get_http_client
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/prompt_ctl",
+    dependencies=[Depends(token_veri)]
+)
 
 @router.post("/prompt_analyze")
-async def prompt_analyze(prompt: str, id_token: str):
-    user_info = await auth.token_verification(id_token)
+async def prompt_analyze(prompt: str, id_token: str, client: app.auth_relation.httpx.AsyncClient = Depends(get_http_client)):
+    user_info = await token_veri(id_token, client=client)
     if user_info is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You need authorize this service")
 
--- frontend/app/home/urls.py
+++ frontend/app/home/urls.py
@@ -1,1 +1,14 @@
 # urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('/', views.welcome, name='visitor'),
+    path('/login', views.login, name='login'),
+    path('/signin', views.signin, name='signin'),
+    path('/dashboard', views.home, name='home'),
+    path('/docs', views.docs, name='docs'),
+    path('/register', views.confirm_schedule, name='register'),
+    path('/edit', views.edit_schedule, name='edit'),
+    path('/delete', views.delete_schedule, name='delete'),
+]
